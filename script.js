32a9ddd06090f70f8aea37a087e662ff69ea5e81
TAHAMOUA <mouaddinetaha@gmail.com>
2026-04-20 21:44:16 +0100

    modefication some comments and parti cart in accueil page add array with id,name,price, image,  for products in js

---
+++ script.js
@@ -0,0 +1,41 @@
+
+//tablue products
+const products = [
+  {
+    id: 1,
+    name: "Huile d'Olive Extra Vierge 250ml",
+    price: 35,
+    image: "image/250ml.png",
+  },
+  {
+    id: 2,
+    name: "Huile d'Olive Extra Vierge 500ml",
+    price: 55,
+    image: "image/500ml.png",
+  },
+  {
+    id: 3,
+    name: "Huile d'Olive Extra Vierge 750ml",
+    price: 70,
+    image: "image/1L.png",
+  },
+  {
+    id: 4,
+    name: "Huile d'Olive Extra Vierge 1L",
+    price: 90,
+    image: "image/bottle.png",
+  },
+  {
+    id: 5,
+    name: "Huile d'Olive Extra Vierge 2L",
+    price: 150,
+    image: "image/2L.png",
+  },
+  {
+    id: 6,
+    name: "Huile d'Olive Extra Vierge 5L",
+    price: 350,
+    image: "image/5L.png",
+  },
+];
+
